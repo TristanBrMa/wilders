164491c39cd762c3972b7ad9961cc7a81f6e2a62
Maya Larribité <tristanbroussemalissard@gmail.com>
2023-05-19 12:49:38 +0200

    chalk time

---
+++ Wilders.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Adrien"));
+console.log(chalk.yellow("Annie"));
+console.log(chalk.red("Le chien et le chat de Cédric"));
+console.log(chalk.green(":chicken:"));
